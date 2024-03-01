bf7737997b11098919bb7339b349d8f45b865c32
atkzw3 <akiyoshi.t3@gmail.com>
2024-03-01 22:10:10 +0900

    :zap: local test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const {request} = require("express");
 const app = express()
 const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("aaaa!!")
+});
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中")
